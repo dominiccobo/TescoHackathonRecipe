39a879b42b626e82c64c446cc9f419a6a8b8c370
Dominic <dominiccobo@gmail.com>
2018-01-11 16:12:41 +0000

    Final service commit

---
+++ backend/src/main/java/uk/ac/brunel/tescohackathonrecipe/service/impl/BasketServiceImpl.java
@@ -0,0 +1,25 @@
+package uk.ac.brunel.tescohackathonrecipe.service.impl;
+
+import org.springframework.stereotype.Service;
+import uk.ac.brunel.tescohackathonrecipe.domain.Basket;
+import uk.ac.brunel.tescohackathonrecipe.service.BasketService;
+
+/**
+ * This a new class.
+ *
+ * @author dom
+ * @since 11/01/2018
+ */
+@Service
+public class BasketServiceImpl implements BasketService {
+
+    @Override
+    public Basket getBasket(String id) {
+        return null;
+    }
+
+    @Override
+    public void updateBasket(Basket basket) {
+
+    }
+}
